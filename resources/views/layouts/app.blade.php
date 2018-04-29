8df96ff04c6197948a6b1a8b5fcaa1b2c6146937
MayLiu <ashucn@gmail.com>
2018-04-29 16:32:30 -0700

    User can upload avatar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
+    <title>@yield('title', 'AshuBBS') - Laravel 进阶教程</title>
 
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
