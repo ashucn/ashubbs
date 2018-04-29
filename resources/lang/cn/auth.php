4e8115e15a1701c566be5e689593912d636c4463
MayLiu <ashucn@gmail.com>
2018-04-29 12:52:39 -0700

    update configuration

---
+++ resources/lang/cn/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => '您的输入不正确，请检查您的输入.',
+    'throttle' => '输入错误次数过多，请在 :seconds 秒后重试.',
+
+];
