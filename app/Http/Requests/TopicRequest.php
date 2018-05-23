c78c2f6aaac0716a6e95ebb7403489995d818473
ashucn <ashucn@qq>
2018-05-22 21:38:35 -0700

    Topic index page

--- app/Http/Requests/TopicRequest.php
+++ app/Http/Requests/TopicRequest.php
@@ -4,31 +4,32 @@ namespace App\Http\Requests;
 
 class TopicRequest extends Request
 {
+
     public function rules()
     {
-        switch ($this->method()) {
+/*        switch ($this->method()) {
             // CREATE
             case 'POST' :
-            {
-                return [
-                    // CREATE ROLES
-                ];
+                {
+                    return [
+                        // CREATE ROLES
+                    ];
             }
             // UPDATE
             case 'PUT':
             case 'PATCH' :
-            {
-                return [
-                    // UPDATE ROLES
-                ];
+                {
+                    return [
+                        // UPDATE ROLES
+                    ];
             }
             case 'GET':
             case 'DELETE':
             default:
-            {
-                return [];
+                {
+                    return [];
             }
-        }
+        }*/
     }
 
     public function messages()
